ab92226a8b1a442b676f0c3a0a0ef0f0a8bd319f
bplower <bplower@gmail.com>
2016-06-26 15:50:38 -0800

    Improved path handling
    It's still a little broken, but it works slightly better than before!

--- src/__main__.py
+++ src/__main__.py
@@ -8,5 +8,16 @@ if len(sys.argv) > 1:
 
 # Generate and set the filetree
 treedict = SimpleWebEditor.get_directory_structure(filename)
-SimpleWebEditor.filetree = SimpleWebEditor.convert_tree(treedict["."])
+# An individual file results in a treedict equal to an empty dictionary
+if treedict != {}:
+	try:
+		SimpleWebEditor.filetree = SimpleWebEditor.convert_tree(treedict[filename])
+	except KeyError:
+		# the root key is the last directory name in the whole path. We need
+		# to replace the key to make loading the file work properly
+		treedict[filename] = treedict[filename.split("/")[-1]]
+		del treedict[filename.split("/")[-1]]
+		SimpleWebEditor.filetree = SimpleWebEditor.convert_tree(treedict[filename])
+else:
+	SimpleWebEditor.filetree = SimpleWebEditor.convert_tree({filename: None})
 SimpleWebEditor.app.run()
